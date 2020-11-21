de2b82e794b7e4ba976c68d12944528cc605a90c
Visine Killian <56771617+VisineK@users.noreply.github.com>
2020-11-21 10:29:04 +0100

    Update Wood_League_1.py

--- Fall Challenge 2020/Wood_League_1.py
+++ Fall Challenge 2020/Wood_League_1.py
@@ -61,7 +61,7 @@ while True:
         # i0: tier-0 ingredients in inventory
         # score: amount of rupees
         # i0, i1, i2, i3, score = [int(j) for j in input().split()]
-        inventory.append(list(map(int, input().split())))[:-1]
+        inventory.append(list(map(int, input().split())))
 
     # Write an at using print
     # To debug: print("Debug messages...", file=sys.stderr, flush=True)
